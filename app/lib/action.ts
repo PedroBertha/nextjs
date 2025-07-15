4825796aff729990888afdb4a5d55077678ff2b9
PedroBertha <phlikoskibertha@gmail.com>
2025-07-15 17:08:48 -0300

    criando segmento de rota dinamico com fatura id

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,9 +1,11 @@
 'use server'
 
 import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import postgres from 'postgres'
 
-const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'})
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
 const FormSchema = z.object({
     id: z.string(),
@@ -24,8 +26,10 @@ export async function createInvoices (formData: FormData) {
     const amountInCents = amount * 100
     const date = new Date(). toISOString().split('T')[0]
     
-    await sql `
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}), ${amountInCents}, ${status}, ${date}
-    `
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
